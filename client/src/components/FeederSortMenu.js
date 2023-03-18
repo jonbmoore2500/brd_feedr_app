a9e5ce3614c4127f668a739901387f3f83a05a14
Jonathan <jonathanmoorevla@gmail.com>
2023-03-18 12:18:13 -0500

    Prep FeederSortMenu component

---
+++ client/src/components/FeederSortMenu.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+function FeederSortMenu() {
+
+    return(
+        <div>
+            <h3>Sort Feeders By: </h3>
+            <form >
+                <select>
+                    
+                </select>
+            </form>
+        </div>
+    )
+
+}
+
+
+export default FeederSortMenu
