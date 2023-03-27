e05bd70baeda47667cc0887eae29550b3932990c
Jonathan <jonathanmoorevla@gmail.com>
2023-03-26 21:38:50 -0500

    Add date display to review cards

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,5 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :bird_id, :feeder_id, :rating, :text
+  attributes :id, :bird_id, :feeder_id, :rating, :text, :updated_at
  
   belongs_to :feeder
 end
--- client/src/components/ReviewCard.js
+++ client/src/components/ReviewCard.js
@@ -3,6 +3,11 @@ import EditRevForm from "./EditRevForm"
 
 function ReviewCard({review, signedIn = false, handleDelete, handleEdit}) {
 
+    let day = review.updated_at.slice(8, 10)
+    let month = review.updated_at.slice(5, 7)
+    let year = review.updated_at.slice(2, 4)
+    let date = `${month}/${day}/${year}`
+
     function starsFunc(num) {
         let solidStars = ["★", "★", "★", "★", "★"].slice(5-num)
         let hollowStars = ["☆", "☆", "☆", "☆", "☆"].slice(num)
@@ -19,8 +24,8 @@ function ReviewCard({review, signedIn = false, handleDelete, handleEdit}) {
             </div>
             <div className="review-contents">
                 <h3>Neighborhood: {review.feeder.neighborhood}</h3>
-                <h3>Rating: {review.rating}</h3>
                 <h3>Review: {review.text}</h3>
+                <h3>Reviewed on: {date}</h3>
                 {signedIn ? (
                     <EditRevForm 
                         review={review} 
