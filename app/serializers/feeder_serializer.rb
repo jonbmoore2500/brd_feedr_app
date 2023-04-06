2a76938c27de79a0f328590590bb94d4aa77d132
Jonathan <jonathanmoorevla@gmail.com>
2023-04-06 18:31:14 -0500

    Refine review controllers to avoid unneeded ifs

--- app/serializers/feeder_serializer.rb
+++ app/serializers/feeder_serializer.rb
@@ -1,6 +1,5 @@
 class FeederSerializer < ActiveModel::Serializer
   attributes :id, :name, :refill_freq, :neighborhood, :average_rating, :num_reviews, :reviews
   
-  # has_many :reviews, serializer: ReviewSerializer
   
 end
